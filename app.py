9146f21ede50fcb63aa515dee7ba710a7e067fbf
Aymen312 <153440336+Aymen312@users.noreply.github.com>
2024-07-16 15:37:10 +0100

    Update app.py
    Signed-off-by: Aymen312 <153440336+Aymen312@users.noreply.github.com>

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from fpdf import FPDF
 import smtplib
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 import os
