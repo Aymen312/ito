15ddf90c0eaf7ff3df8c255f666c3e0e3bd1b560
Aymen312 <153440336+Aymen312@users.noreply.github.com>
2025-04-10 23:22:57 +0200

    Update app.py
    Signed-off-by: Aymen312 <153440336+Aymen312@users.noreply.github.com>

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def display_supplier_info(df, fournisseur):
 
 def display_designation_info(df, designation):
     # Colonnes à afficher dans le tableau
-    colonnes_a_afficher = ['barcode', 'taille', 'Qté stock dispo', 'designation']
+    colonnes_a_afficher = ['barcode', 'taille', 'designation', 'Qté stock dispo']
     designation = designation.strip().upper()
     df['designation'] = df['designation'].fillna('')
     
