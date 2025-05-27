3a4f6a79c3d7f7d73425fb8ae4f8bcf44a65652b
Aymen312 <153440336+Aymen312@users.noreply.github.com>
2025-05-27 23:06:01 +0100

    Update app.py
    Signed-off-by: Aymen312 <153440336+Aymen312@users.noreply.github.com>

--- app.py
+++ app.py
@@ -415,17 +415,18 @@ def display_stock_by_family(df):
 def display_specific_designations(df):
     # Dictionnaire des fournisseurs et leurs modèles
     suppliers = {
-    "ASICS": [
-        "GEL-CUMULUS 27", "GEL-CUMULUS 27 W", "GEL-TRABUCO 13 GTX", "GEL-TRABUCO 13 GTX W",
-        "GT-2000 13 TR", "GT-2000 13 TR W", "GT-2000 13 W",
-        "MAGIC SPEED 4", "METASPEED EDGE+", "NOVABLAST 5", "NOVABLAST 5 W",
-        "NOOSA TRI 16", "NOOSA TRI 16 W", "GEL-NIMBUS 27", "GEL-NIMBUS 27 W"
-    ],
+        "ASICS": [
+            "GEL-CUMULUS 27", "GEL-CUMULUS 27 W", "GEL-TRABUCO 13 GTX", "GEL-TRABUCO 13 GTX W",
+            "GT-2000 13 TR", "GT-2000 13 TR W", "GT-2000 13 W",
+            "MAGIC SPEED 4", "METASPEED EDGE+", "NOVABLAST 5", "NOVABLAST 5 W",
+            "NOOSA TRI 16", "NOOSA TRI 16 W", "GEL-NIMBUS 27", "GEL-NIMBUS 27 W"
+        ],
         "BROOKS": [
             "CALDERA 8", "CALDERA 8 W", "CASCADIA 18", "CASCADIA 18 GTX",
-            "CASCADIA 18 GTX W", "CASCADIA 18 W", "CASCADIA 18 W",
+            "CASCADIA 18 GTX W", "CASCADIA 18 W",
             "GHOST 16", "GHOST 16 W", "GHOST MAX 2", "GHOST MAX 2 W",
-            "GLYCERIN 22", "GLYCERIN 22 W", "HYPERION 2", "HYPERION MAX 2"
+            "GLYCERIN 22", "GLYCERIN 22 W", "HYPERION 2", "HYPERION MAX 2",
+            "GHOST 17", "GHOST 17 W"  # Ajout pour Brooks
         ],
         "HOKA": [
             "SPEEDGOAT 6", "SPEEDGOAT 6 W",
@@ -448,7 +449,7 @@ def display_specific_designations(df):
         ],
         "NEW BALANCE": [
             "880 V15", "880 V15 W", "REBEL V4",
-            "FUELCELL REBEL"  # Added FUELCELL REBEL model
+            "FUELCELL REBEL"
         ],
         "SALOMON": [
             "ULTRA GLIDE 3", "ULTRA GLIDE 3 W",
@@ -458,8 +459,10 @@ def display_specific_designations(df):
             "ENDORPHIN PRO 4", "ENDORPHIN SPEED 4", "ENDORPHIN SPEED 4 W",
             "KINVARA 15", "KINVARA 15 W", "PEREGRINE 15", "PEREGRINE 15 W",
             "RIDE 18", "RIDE 18 W", "RIDE TR2", "RIDE TR2 W",
-            "TRIUMPH 22", "TRIUMPH 22", "TRIUMPH 22 W",
-             "XODUS ULTRA 3", "XODUS ULTRA 3 W"
+            "TRIUMPH 22", "TRIUMPH 22 W",
+            "XODUS ULTRA 3", "XODUS ULTRA 3 W",
+            "XODUS ULTRA 4", "XODUS ULTRA 4 W",  # Ajout pour Saucony
+            "TRIUMPH 23", "TRIUMPH 23 W"      # Ajout pour Saucony
         ]
     }
 
